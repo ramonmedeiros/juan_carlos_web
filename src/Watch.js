d76f5d2d906011dadf2ee8992e20cf20b412402b
Ramon Medeiros <ramon.medeiros@voiapp.io>
2021-04-18 19:18:49 +0200

    Remove failed trackers

--- src/Watch.js
+++ src/Watch.js
@@ -30,7 +30,7 @@ class Watch extends Component {
     }
 
     generateMagnetLink(infoHash) {
-        return `magnet:?xt=urn:btih:${infoHash}&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com`
+        return `magnet:?xt=urn:btih:${infoHash}&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.openwebtorrent.com`
     }
 
     render() {
