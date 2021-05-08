af91ce45656610510bd8abda146b5e0588aecf1d
Ramon Medeiros <ramon@ramonmedeiros.dev>
2021-05-08 21:13:59 +0000

    Updates

--- cleanup.py
+++
@@ -1,6 +0,0 @@
-import pandas
-df = pandas.read_csv("title.basics.tsv", sep='\t')
-df = df.drop(columns=['isAdult','startYear', 'endYear', 'runtimeMinutes', 'genres'])
-df = df.query('titleType == "movie"')
-df = df.drop(columns=['titleType', 'primaryTitle'])
-df.to_csv("movies.csv")
